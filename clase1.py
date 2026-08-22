c82611a41ac05b4b7d9fb80d15da0e67abc796be
Aulas Ingenieria <aulasingenieria@ujaverianas.loc>
2026-08-22 11:54:59 -0500

    con match, case _: ##default

--- clase1.py
+++ clase1.py
@@ -10,3 +10,28 @@ elif age <= 18:
     print("Eres un joven")
 else:
     print("Eres un viejo")
+
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+print("1. suma")
+print("2. resta")
+print("3. multiplicacion")
+print("4. dividir")
+
+oper = int(input("Ingrese la operacion a realizar: "))
+
+match oper:
+    case 1:
+        print(num1 + num2)
+
+    case 2:
+        print(num1 - num2)
+
+    case 3:
+        print(num1 * num2)
+
+    case 4:
+        print(num1 / num2)
+
+    case _:
+        print("Operacion invalido")
