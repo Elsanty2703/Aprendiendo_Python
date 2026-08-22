e0c60f643509448b28ea8f0d6ac45db0eb2adc79
Aulas Ingenieria <aulasingenieria@ujaverianas.loc>
2026-08-22 11:45:12 -0500

    primer ejercicio

---
+++ clase1.py
@@ -0,0 +1,10 @@
+age = int(input("Cuantos años tiene: "))
+
+if age <= 8:
+    print("Eres un bebe")
+elif age <= 12:
+    print("Eres un niño")
+elif age <= 18:
+    print("Eres un joven")
+else:
+    print("Eres un viejo")
